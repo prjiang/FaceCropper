2ad1723d9bd9c0dc1fe9f9d4ce4b1add97b2559b
P.R. Jiang <59597823+prjiang@users.noreply.github.com>
2021-05-26 14:21:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,10 +8,12 @@ while True:
     
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     
+    '''
     faces = face_cascade.detectMultiScale(gray, 1.1, 4)     
     
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)  
+    '''
     
     cv2.namedWindow('img', cv2.WINDOW_NORMAL)
     cv2.imshow('img', frame)
